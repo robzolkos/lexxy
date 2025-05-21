1f5e512fac6a7237719d13ab787ec8a4234f97d3
Jorge Manrubia <jorge@37signals.com>
2025-05-21 22:46:43 +0200

    Format

--- src/helpers/storage_helper.js
+++ src/helpers/storage_helper.js
@@ -1,7 +1,7 @@
 export function bytesToHumanSize(bytes) {
   if (bytes === 0) return "0 B"
-  const sizes = ["B", "KB", "MB", "GB", "TB", "PB"]
+  const sizes = [ "B", "KB", "MB", "GB", "TB", "PB" ]
   const i = Math.floor(Math.log(bytes) / Math.log(1024))
   const value = bytes / Math.pow(1024, i)
-  return `${value.toFixed(2)} ${sizes[i]}`
+  return `${ value.toFixed(2) } ${ sizes[i] }`
 }
