193cf345fc643d141d81db029e5b8b81b4e8a514
Jorge Manrubia <jorge@37signals.com>
2025-05-31 16:56:55 +0200

    Add some tests for event listening

---
+++ test/system/events_test.rb
@@ -0,0 +1,26 @@
+require "application_system_test_case"
+
+class EventsTest < ApplicationSystemTestCase
+  test "no actiontext:change event on initial load" do
+    visit edit_post_path(posts(:empty))
+
+    assert_no_dispatched_event "actiontext:change"
+  end
+
+  test "dispatch actiontext:change event on edits" do
+    visit edit_post_path(posts(:empty))
+
+    find_editor.send "Y"
+
+    assert_dispatched_event "actiontext:change"
+  end
+
+  private
+    def assert_dispatched_event(type)
+      assert_selector "[data-event='#{type}']"
+    end
+
+    def assert_no_dispatched_event(type)
+      assert_no_selector "[data-event='#{type}']"
+    end
+end
