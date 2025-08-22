7d68f5818d883830e622ed610e35c8833ae997ff
Jorge Manrubia <jorge@37signals.com>
2025-08-22 10:29:57 +0200

    Remove typo

--- lib/actiontext/lexical/action_text_tag.rb
+++ lib/actiontext/lexical/action_text_tag.rb
@@ -12,7 +12,6 @@ module ActionText
 
         add_default_name_and_id(options)
         options["input"] ||= dom_id(object, [ options["id"], :trix_input ].compact.join("_")) if object
-        options[""] ||= dom_id(object, [ options["id"], :trix_input ].compact.join("_")) if object
         html_tag = @template_object.rich_textarea_tag(options.delete("name"), options.fetch("value") { value }, options.except("value"), &@block)
         error_wrapping(html_tag)
       end
