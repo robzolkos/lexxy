f4dff89c7b44e62a0585c3d9b4d5ed0ea5800cd0
MSILycanthropy <ethanmichaelk@gmail.com>
2025-09-10 09:06:08 -0500

    refactor: move `lexxy:file-accept` tests

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -15,6 +15,26 @@ class EventsTest < ApplicationSystemTestCase
     assert_dispatched_event "lexxy:change"
   end
 
+  test "use lexxy:file-accept to allow valid attachments" do
+    visit edit_post_path(posts(:empty), attachment_type: "image/png")
+
+    attach_file file_fixture("example.png") do
+      click_on "Upload file"
+    end
+
+    assert_image_figure_attachment content_type: "image/png", caption: "example.png"
+  end
+
+  test "use lexxy:file-accept to block invalid attachments" do
+    visit edit_post_path(posts(:empty), attachment_type: "image/png")
+
+    attach_file file_fixture("note.txt") do
+      click_on "Upload file"
+    end
+
+    assert_no_attachment content_type: "text/plain"
+  end
+
   private
     def assert_dispatched_event(type)
       assert_selector "[data-event='#{type}']"
