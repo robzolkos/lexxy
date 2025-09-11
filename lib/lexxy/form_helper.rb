f8d9b89360d2d1154b6bbbb13b0feed63617cc67
Jorge Manrubia <jorge@37signals.com>
2025-09-11 15:47:52 +0200

    Fix: lexxy form helper not working (when disabling action text overrides)

--- lib/lexxy/form_helper.rb
+++ lib/lexxy/form_helper.rb
@@ -1,7 +1,7 @@
 module Lexxy
   module FormHelper
     def lexxy_rich_textarea(object_name, method, options = {}, &block)
-      ActionView::Helpers::Tags::ActionText.new(object_name, method, self, options, &block).render
+      ActionView::Helpers::Tags::ActionText.new(object_name, method, self, options, &block).lexxy_render
     end
 
     alias_method :lexxy_rich_text_area, :lexxy_rich_textarea
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -25,5 +25,7 @@ module Dummy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.lexxy.override_action_text_defaults = false
   end
 end
