767e4a4c7f7a55d21cdd084a8407ba3f30588c5f
Jorge Manrubia <jorge@37signals.com>
2025-09-11 15:52:50 +0200

    Not meant to commit this

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -25,7 +25,5 @@ module Dummy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.lexxy.override_action_text_defaults = false
   end
 end
