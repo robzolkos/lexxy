f087c66dff2b5063bccf9a31e06455c693081550
Tony Messias <tonysm@hey.com>
2025-09-15 22:48:36 -0300

    Adds a test

--- test/system/paste_test.rb
+++ test/system/paste_test.rb
@@ -16,4 +16,12 @@ class PateTest < ApplicationSystemTestCase
     find_editor.paste "https://37signals.com"
     assert_equal_html %(<p>Hello <a href="https://37signals.com">everyone</a></p>), find_editor.value
   end
+
+  test "create links when pasting URLs keeps formatting" do
+    visit edit_post_path(posts(:hello_world))
+    find_editor.select("everyone")
+    find_editor.toggle_command("bold")
+    find_editor.paste "https://37signals.com"
+    assert_equal_html %(<p>Hello <a href="https://37signals.com"><b><strong>everyone</strong></b></a></p>), find_editor.value
+  end
 end
