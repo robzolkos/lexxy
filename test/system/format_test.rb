97c6fd1c46e4ba1026e2b7cc3bbfdb281678ed95
Tony Messias <tonysm@hey.com>
2025-09-19 11:40:06 -0300

    Fix format test

--- test/system/format_test.rb
+++ test/system/format_test.rb
@@ -9,6 +9,7 @@ class FormatTest < ApplicationSystemTestCase
     find_editor.toggle_command("italic")
 
     click_on "Update Post"
+    click_on "Edit this post"
 
     assert_equal_html "<p>Hello <i><b><strong>everyone</strong></b></i></p>", find_editor.value
     assert_equal_html "<p dir=\"ltr\"><span data-lexical-text=\"true\">Hello </span><strong class=\"lexxy-content__bold lexxy-content__italic\" data-lexical-text=\"true\">everyone</strong></p>", find_editor.inner_html
